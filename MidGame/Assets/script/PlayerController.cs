b60fb9860abc5999aa9f4ab2cb00c708f56f298e
mjun6356 <mjun6356@g.shingu.ac.kr>
2026-04-21 19:34:52 +0900

    임시저장

--- MidGame/Assets/script/PlayerController.cs
+++ MidGame/Assets/script/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
+    
+    public float baseSpeed = 6f;
+    public float moveSpeed = 3f;
+    public float jumpForce = 3f;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
@@ -15,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
 
     private bool isGiant = false;
+    private bool isMoving = false;
 
     private float moveInput;
 
@@ -27,7 +30,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput * baseSpeed, rb.linearVelocity.y);
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector2.right * moveX * moveSpeed * Time.deltaTime);
 
         if (isGiant)
         {
@@ -44,17 +50,30 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        
+        /*if (isMoving)
+        {
+            if (moveInput < 0)
+                moveSpeed = 6f;
+            else if (moveInput > 0)
+                moveSpeed = 6f;
+        }
+        else
+        {
+            if (moveInput < 0)
+                moveSpeed = 6f;
+            else if (moveInput > 0)
+                moveSpeed = 6f;
+        }*/
 
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    public void OnMove(InputValue value)
+    /*public void OnMove(InputValue value)
     {
         Vector2 intput = value.Get<Vector2>();
         moveInput = intput.x;
-    }
+    }*/
 
     public void OnJump(InputValue value)
     {
@@ -93,11 +112,21 @@ public class PlayerController : MonoBehaviour
             Destroy(collision.gameObject);
         }
 
+        if (collision.CompareTag("Item2"))
+        {
+            isMoving = true;
+            Invoke(nameof(ResetMoving), 6f);
+            Destroy(collision.gameObject);
+        }
     }
 
    void ResetGiant()
     {
         isGiant = false;
+    }
+    void ResetMoving()
+    {
+        isMoving = false;
     }
 
 }
